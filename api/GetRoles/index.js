13d82f7497a8a4328c193318feaeceec0387c654
mchangchien <matt.changchien@microsoft.com>
2025-03-23 19:56:59 +0800

    simplify

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -8,14 +8,13 @@ const roleGroupMappings = {
 
 module.exports = async function (context, req) {
     const user = req.body || {};
-    const roles = [];
+    const roles = ["XXX"];
     
-    for (const [role, groupId] of Object.entries(roleGroupMappings)) {
-        if (await isUserInGroup(groupId, user.accessToken)) {
-            roles.push(role);
-        }
-    }
-
+    // for (const [role, groupId] of Object.entries(roleGroupMappings)) {
+    //     if (await isUserInGroup(groupId, user.accessToken)) {
+    //         roles.push(role);
+    //     }
+    // }
     context.res.json({
         roles
     });
